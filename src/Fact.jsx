4793872b7c55b5568a5ae08b182cf1969056a4f6
Marco Fornaciari <mfornaciari@gmail.com>
2022-08-26 22:53:22 -0300

    fix: change label in Fact to span

--- src/Fact.jsx
+++ src/Fact.jsx
@@ -1,6 +1,6 @@
 import '../style/style.css';
 
-function Fact ({content, onChange}) {
+function Fact({ content, onChange }) {
   return (
     <li>
       <input
@@ -10,9 +10,9 @@ function Fact ({content, onChange}) {
         checked={content.read ? true : false}
         className='checkbox'
       />
-      <label htmlFor='factCheckbox' className={content.read ? 'read' : null}>
+      <span className={content.read ? 'read' : null}>
         {content.text}
-      </label>
+      </span>
     </li>
   );
 }
