7697b1fcbdcbc6a6f469373cb39c1a32cff6b8d5
Marco Fornaciari <marco.fornaciari@rebase.com.br>
2022-08-29 16:58:37 -0300

    feat: fetch data from API

--- src/FactList.jsx
+++ src/FactList.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Fact from './Fact';
 import HideReadButton from './HideReadButton';
 
 function FactList() {
-  const [facts, setFacts] = useState(
-    [
-      { id: 0, text: 'Was first released in 2013', read: false },
-      { id: 1, text: 'Has well over 100K stars on Github', read: false },
-      { id: 2, text: 'Is maintained by Facebook', read: false },
-      { id: 3, text: 'Powers thousands of enterprise apps, including mobile apps', read: false }
-    ]
-  );
+  const [facts, setFacts] = useState([]);
   const [hideRead, setHideRead] = useState(false);
 
   const filteredFacts = hideRead ? facts.filter(fact => !fact.read) : facts;
@@ -21,6 +14,16 @@ function FactList() {
     });
     setFacts(updatedFacts);
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://run.mocky.io/v3/20048106-9506-46bb-82ac-9465296402b2');
+      const jsonResponse = await response.json();
+      const data = jsonResponse.facts.map(fact => ({...fact, read: false}));
+      setFacts(data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,8 +1,18 @@
-import { fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../src/App';
 
 describe('App', () => {
+  const mockResponse = Promise.resolve({
+    json: () => Promise.resolve({facts: [
+      { id: 0, text: 'Was first released in 2013'},
+      { id: 1, text: 'Has well over 100K stars on Github'},
+      { id: 2, text: 'Is maintained by Facebook'},
+      { id: 3, text: 'Powers thousands of enterprise apps, including mobile apps'},
+    ]})
+  });
+  global.fetch = jest.fn(() => mockResponse);
+
   const factsText = [
     'Was first released in 2013',
     'Has well over 100K stars on Github',
@@ -14,12 +24,13 @@ describe('App', () => {
     render(<App />);
   });
 
-  it('renders correctly', () => {
+  it('renders correctly', async () => {
     const title = screen.getByRole('heading');
     const hideReadButton = screen.getByRole('button');
-    const facts = screen.getAllByRole('listitem');
-    const checkboxes = screen.getAllByRole('checkbox');
+    const facts = await screen.findAllByRole('listitem');
+    const checkboxes = await screen.findAllByRole('checkbox');
 
+    expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(title.textContent).toBe('Fun facts about React');
     expect(hideReadButton.textContent).toBe('Hide read facts');
     for (const [index, fact] of facts.entries()) {
@@ -30,19 +41,19 @@ describe('App', () => {
     }
   });
 
-  it('puts line through fact text when checkbox is clicked', () => {
-    const facts = screen.getAllByRole('listitem');
-    const checkboxes = screen.getAllByRole('checkbox');
+  it('puts line through fact text when checkbox is clicked', async () => {
+    const facts = await screen.findAllByRole('listitem');
+    const checkboxes = await screen.findAllByRole('checkbox');
 
     for (const checkbox of checkboxes) { fireEvent.click(checkbox) }
 
     for (const fact of facts) { expect(fact).toHaveClass('read') }
   });
 
-  it('hides all read facts and changes button text when button is clicked while read facts are shown', () => {
+  it('hides all read facts and changes button text when button is clicked while read facts are shown', async () => {
     const hideReadButton = screen.getByRole('button');
 
-    for (const checkbox of screen.getAllByRole('checkbox')) { fireEvent.click(checkbox) }
+    for (const checkbox of await screen.findAllByRole('checkbox')) { fireEvent.click(checkbox) }
     fireEvent.click(hideReadButton);
 
     expect(hideReadButton.textContent).toBe('Show read facts');
@@ -50,10 +61,10 @@ describe('App', () => {
     for (const checkbox of screen.queryAllByRole('checkbox')) { expect(checkbox).not.toBeInTheDocument() }
   });
 
-  it('shows all read facts and resets button text when button is clicked while read facts are hidden', () => {
+  it('shows all read facts and resets button text when button is clicked while read facts are hidden', async () => {
     const hideReadButton = screen.getByRole('button');
 
-    for (const checkbox of screen.getAllByRole('checkbox')) { fireEvent.click(checkbox) }
+    for (const checkbox of await screen.findAllByRole('checkbox')) { fireEvent.click(checkbox) }
     fireEvent.click(hideReadButton); // Hides read facts
     fireEvent.click(hideReadButton); // Shows read facts
 
