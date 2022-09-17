3dfcc946b7de97d04c834b6edccfb71681cc7964
Mat <mateuscolvr@gmail.com>
2022-09-17 10:57:13 -0300

    conselho do doutor

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,11 +1,11 @@
-import App from "../src/App"
-import {screen, render} from "@testing-library/react"
-import "@testing-library/jest-dom"
+import App from "../src/App";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("App", () => {
-  const { container } = render(<App/>);
+  const { container } = render(<App />); // Don't do this! Check this link https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#using-container-to-query-for-elements
 
   it("should show Oi", () => {
-    expect(container.textContent).toEqual("Oi")
-  })
-})
+    expect(container.textContent).toEqual("Oi"); // https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#using-the-wrong-assertion
+  });
+});
